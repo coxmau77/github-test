0119a6762344d707174a323c4bee55c1fee7f67c
coxmau77 <coxmau77@gmail.com>
2023-06-06 07:57:23 -0300

    Personalizacion de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    
+    colors: {
+      mi_color: 'rgb(255, 167, 0)'
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
