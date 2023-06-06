207b0479330deb8e3ec5bebe6f23bc09920962f4
coxmau77 <coxmau77@gmail.com>
2023-06-06 08:56:20 -0300

    No funcionan las clases de bg revisar porque tampoco la clase rounded-xl

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # github-test
+```bash
+npx tailwindcss -i ./input.css -o ./public/style.css --watch
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+
+    fontFamily:{
+      mi_fuente: ['Kurale'],
+    },
     
     colors: {
-      mi_color: 'rgb(255, 167, 0)'
+      mi_color: 'rgb(255, 167, 0)',
+      color_gradient:{
+        100: '#87CEFA',
+        200: '#7B68EE',
+        300: '#0000FF'
+      }
     },
 
     extend: {},
